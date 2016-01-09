6b4af6f8542f198fd00facffd79074a2444e3d21
Serguey Arellano <sarellano85@gmail.com>
2016-01-09 23:24:17 +0100

    add questions html for client, standby

--- public/client/app.js
+++ public/client/app.js
@@ -1,7 +1,9 @@
 questions = {
-	q1:{title:'Question one, vote!', answerA:'vote Good',answerB:'vote Bad'},
-	q2:{title:'Question two', answerA:'Better',answerB:'Worse'},
-	q3:{title:'Question three', answerA:'Batman',answerB:'Robin'}
+	q0:{title:'Question one, vote!', answerA:'vote Good',answerB:'vote Bad'},
+	q1:{title:'Question two, vote!', answerA:'vote Good',answerB:'vote Bad'},
+	q2:{title:'Question three', answerA:'Better',answerB:'Worse'},
+	q3:{title:'Question four, vote!', answerA:'vote Good',answerB:'vote Bad'},
+	q4:{title:'Question five', answerA:'Batman',answerB:'Robin'}
 }
 
 // Init client socket interface
@@ -15,7 +17,10 @@ ws.onmessage = function (event) {
 
 		switch (data.type) {
 			case 'nextQuestion':
-				window.location.href = '/welcome/q1';
+				window.location.href = '/welcome/' + data.value;
+				break;
+			case 'standBy':
+				window.location.href = '/welcome/standBy';
 				break;
 			case 'connected':
 				if (!!document.querySelector('#pings p')) {
@@ -32,13 +37,36 @@ angular.module('welcomeApp',['welcomeRoutes'])
 	.controller('mainController', function() {
 		var vm = this;
 
-		vm.sendPoll = function ($event) {
-			ws.send(JSON.stringify({type:'poll',id:$event.target.id, value:$event.target.value}));
+		vm.sendPoll = function ($event, results, index) {
+			ws.send(JSON.stringify({type:'poll',results:results, index:index, value:$event.target.value}));
 		}
 	})
 
+	.controller('q0Controller', function() {
+		var vm = this;
+
+		vm.q0 = questions.q0;
+	})
 	.controller('q1Controller', function() {
 		var vm = this;
 
 		vm.q1 = questions.q1;
+	})
+	.controller('q2Controller', function() {
+		var vm = this;
+
+		vm.q2 = questions.q2;
+	})
+	.controller('q3Controller', function() {
+		var vm = this;
+
+		vm.q3 = questions.q3;
+	})
+	.controller('q4Controller', function() {
+		var vm = this;
+
+		vm.q4 = questions.q4;
+	})
+	.controller('standByController', function() {
+		var vm = this;
 	});
