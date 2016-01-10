16a6944da645cb0f195e700a32e783b9caee3fbb
Serguey Arellano <sarellano85@gmail.com>
2016-01-10 12:55:08 +0100

    add start and stop poll buttons functionality

--- public/admin/app.js
+++ public/admin/app.js
@@ -34,18 +34,39 @@ ws.onmessage = function (event) {
 angular.module('adminApp',['adminRoutes'])
 	.controller('mainController', function() {
 		var vm = this;
-		vm.results = {r0:false,r1:false,r2:false,r3:false,r4:false,}
-		vm.startPoll = function($event,_r) {
+		vm.results = {r0:true,r1:false,r2:false,r3:false,r4:false}
+		vm.started = false;
+		vm.pollState = 'Pulsa Start para comenzar con la votacion'
+		vm.showPoll = function(_r) {
+
 			for (r in vm.results) {
 				vm.results[r] = false;
 			}
-			vm.results[_r] = true;
-			console.log(vm.results);
 
-			ws.send(JSON.stringify({type:'nextQuestion',value:$event.target.htmlFor}));
+			vm.results[_r] = true;
 		};
-		vm.standBy = function() {
-			ws.send(JSON.stringify({type:'standBy'}));
-		}
+		vm.startPoll = function($event) {
+
+			if (!vm.started) {
+
+				for (r in vm.results) {
+					if (vm.results[r] === true) {
+
+						ws.send(JSON.stringify({type:'nextQuestion',value:r}));
+						vm.pollState = 'La votacion ha comenzado :)';
+						vm.started = true;
+					}
+				}
+			}
+		};
+		vm.stopPoll = function($event) {
+
+			if (vm.started) {
+
+				ws.send(JSON.stringify({type:'standBy'}));
+				vm.pollState = 'No hay ninguna votacion en curso :(';
+				vm.started = false;
+			}
+		};
 	});
 
