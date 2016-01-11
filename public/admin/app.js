678f2b13d97e889c1c4d373a75a7611a3cef8cdc
Serguey Arellano <serguey.arellano.contractor@bbva.com>
2016-01-11 14:51:47 +0100

    add directive pollstar

--- public/admin/app.js
+++ public/admin/app.js
@@ -1,13 +1,7 @@
-questions = {
-	q1:{title:'Question one results', answerA:'barra graph Good',answerB:'barra graph Bad'},
-	q2:{title:'Question two', answerA:'Better',answerB:'Worse'},
-	q3:{title:'Question three', answerA:'Batman',answerB:'Robin'}
-}
-
 // Init admin socket interface
 var host = location.origin.replace(/^http/, 'ws');
 var ws = new WebSocket(host);
-
+var totalVotes = {results0:0,results1:0,results2:0,results3:0,results4:0};
 // listen to messages
 ws.onmessage = function (event) {
 
@@ -21,7 +15,8 @@ ws.onmessage = function (event) {
 			}
 			break;
 		case 'poll':
-
+			totalVotes[data.results] += 1;
+			console.log(totalVotes); 
 			var val = document.getElementById(data.results.toString()).children[data.index].innerHTML;
 			val = parseInt(val) + parseInt(data.value);
 			document.getElementById(data.results.toString()).children[data.index].innerHTML = val;
@@ -31,12 +26,24 @@ ws.onmessage = function (event) {
 	}
 }
 
+var pollResults = {
+	r0: {oneStar: 1, twoStar:0, threeStar:0, fourStar:0, fiveStar: 0},
+	r1: {oneStar: 0, twoStar:0, threeStar:0, fourStar:0, fiveStar: 0},
+	r2: {oneStar: 0, twoStar:0, threeStar:0, fourStar:0, fiveStar: 0},
+	r3: {oneStar: 0, twoStar:0, threeStar:0, fourStar:0, fiveStar: 0},
+	r4: {oneStar: 0, twoStar:0, threeStar:0, fourStar:0, fiveStar: 0},
+	value: function(r) {
+		return this.r.oneStar
+	}
+}
+
 angular.module('adminApp',['adminRoutes'])
 	.controller('mainController', function() {
 		var vm = this;
-		vm.results = {r0:true,r1:false,r2:false,r3:false,r4:false}
+		vm.pollResults = pollResults;
+		vm.results = {r0:true,r1:false,r2:false,r3:false,r4:false};
 		vm.started = false;
-		vm.pollState = 'Pulsa Start para comenzar con la votacion'
+		vm.pollState = 'Pulsa Start para comenzar con la votacion';
 		vm.showPoll = function(_r) {
 
 			for (r in vm.results) {
@@ -68,5 +75,29 @@ angular.module('adminApp',['adminRoutes'])
 				vm.started = false;
 			}
 		};
+	})
+	.directive('pollstar', function() {
+	  	return {
+	      restrict: 'E',
+	      transclude: true,
+	      scope: {},
+	      link: function($scope, element, attributes){
+	      		$scope.myclass = attributes.myclass;
+	      		$scope.myvalue = attributes.myvalue;
+
+	      },
+	      controller: function($scope, $element) {},
+	      template:
+	       	'<div class="{{myclass}}" aria-valuemin="0" aria-valuemax="100" aria-valuenow="{{myvalue}}" role="progressbar">' +
+					'<span class="left-mask">' +
+					'<span class="circle"></span>' +
+					'</span>' +
+					'<span class="right-mask">' +
+					'<span class="circle"></span>' +
+					'</span>' +
+					'<div class="circle icon star"></div>' +
+					'</div>',
+	      replace: true
+	    };
 	});
 
