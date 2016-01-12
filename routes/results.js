8cfeb9e085a3aa8950eaa5c2a8f9ef2a962fce52
Serguey Arellano <serguey.arellano.contractor@bbva.com>
2016-01-12 17:27:13 +0100

    add path _results_

---
+++ routes/results.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET client */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../public/admin/views/index.html'));
+});
+
+module.exports = router;
