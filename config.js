7114f552e2a6437e4c350a454d8dfcceda013862
Serguey Arellano <sarellano85@gmail.com>
2016-01-25 23:54:03 +0100

    add database replica mongoDB URI

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   'port': process.env.PORT || 3000,
-  'database': 'mongodb://livefeedback:admin@ds043615.mongolab.com:43615/livefeedback',
+  // 'database': 'mongodb://livefeedback:admin@ds043615.mongolab.com:43615/livefeedback'
+  'database': 'mongodb://livefeedback:admin@ds047901.mongolab.com:47901/livefeedbackreplica'
 }
