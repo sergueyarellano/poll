67144c25efba51c8add2e802624d396c166658df
Serguey Arellano <sarellano85@gmail.com>
2016-01-25 23:56:22 +0100

    change mongoose connect to database replica

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function(req, res, next) {
 //}));
 
 // connect to the database
-mongoose.connect(config.database);
+mongoose.connect(config.databaseReplica);
 
 app.use(express.static(path.join(__dirname, 'public/')));
 
@@ -69,13 +69,13 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-          message: err.message,
-          error: err
-      });
-  });
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
 // production error handler
