73f897a56722edb7bd38a53b75bbcb471c3dfb65
Serguey Arellano <serguey.arellano.contractor@bbva.com>
2016-04-07 18:14:11 +0200

    add style and ip-filter package

--- routes/welcome.js
+++ routes/welcome.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const ipFilter = require('ip-filter');
 
 /* GET client */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/../public/client/views/index.html'));
+    var ipArray = req._remoteAddress.split(':')[3].split('.');
+
+    console.log('request',req._remoteAddress.split(':')[3]);
+    if (parseInt(ipArray[0]) === 127 && parseInt(ipArray[2]) === 0) {
+        res.send({
+            message: 'Esta Ip no tiene permitido el acceso',
+            ip: req._remoteAddress.split(':')[3]
+
+    });
+    } else {
+
+        res.sendFile(path.join(__dirname + '/../public/client/views/index.html'));
+    }
 });
 
 module.exports = router;
