e870c0d3c72cb3d22d2ed61db571c7f93c478f48
jofralogo <jofralogo@gmail.com>
2016-04-12 14:40:15 +0200

    add default ip to welcome js

--- routes/welcome.js
+++ routes/welcome.js
@@ -8,7 +8,7 @@ const ipFilter = require('ip-filter');
 
 /* GET client */
 router.get('/', function(req, res, next) {
-    var reqIP = req.headers['x-forwarded-for'] || req._remoteAddress.split(':')[3];
+    var reqIP = req.headers['x-forwarded-for'] || req._remoteAddress.split(':')[3] || '127.0.0.1';
 //https://nodejs.org/docs/latest/api/url.html#url_url_format_urlobj
     if (ipFilter(reqIP, ['127.0.0.1']) ||
         ipFilter(reqIP, ['89.107.180.*']) ||
